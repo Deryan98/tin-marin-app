b422505d4255e78c2253fe2eaa153d84cbcb2539
Cristian Mundo <00086917@uca.edu.sv>
2020-12-07 20:04:00 -0600

    proyect created

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>solo he creado el proyecto.</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
+
+
