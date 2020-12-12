68c9cd15684cf37d706785225a7e537fcadb0df7
Deryan98 <00125116@uca.edu.sv>
2020-12-11 23:43:45 -0600

    Interfaz de formulario Completada

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -1,94 +1,90 @@
-import React from 'react'
-import {createStackNavigator} from "@react-navigation/stack"
-import { StyleSheet, Text, View } from 'react-native'
-import {IconButton} from "react-native-paper";
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {StyleSheet, Text, View} from 'react-native';
+import {IconButton} from 'react-native-paper';
 
 //Screens
-import HomeScreen from "../screens/HomeScreen"
+import HomeScreen from '../screens/HomeScreen';
 import ExhibitsScrn from '../screens/ExhibitsScreen';
-import EventsScreen from "../screens/EventsScreen";
-import CovidScreen from "../screens/CovidScreen";
-import ContactScreen from "../screens/ContactScreen";
-import SugerenceScreen from "../screens/SugerenceScreen";
-import DonationScreen from "../screens/DonationScreen"
-
+import EventsScreen from '../screens/EventsScreen';
+import CovidScreen from '../screens/CovidScreen';
+import ContactScreen from '../screens/ContactScreen';
+import SugerenceScreen from '../screens/SugerenceScreen';
+import DonationScreen from '../screens/DonationScreen';
 
 const Stack = createStackNavigator();
-export default function StackNavigation (props){
-    const {navigation} = props
-    const buttonLeft = () =>{
-        return (
-            <IconButton
-                icon="menu"
-                onPress={()=>navigation.openDrawer()}
-                color="#4E73DF"
-            />
-        )
-    }
-
+export default function StackNavigation(props) {
+  const {navigation} = props;
+  const buttonLeft = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="home"
-                component={HomeScreen}
-                options={{
-                    title: '',
-                    headerLeft: ()=> buttonLeft(),
-                    
-                }}
-            />
-            <Stack.Screen
-                name="exhibits"
-                component={ExhibitsScrn}
-                options={{
-                    title: '',
-                    headerLeft: ()=> buttonLeft()
-                }}
-            />
-            <Stack.Screen
-                name="events"
-                component={EventsScreen}
-                options={{
-                    title: '',
-                    headerLeft: ()=> buttonLeft()
-                }}
-            />
-            <Stack.Screen
-                name="covid"
-                component={CovidScreen}
-                options={{
-                    title: '',
-                    headerLeft: ()=> buttonLeft()
-                }}
-            />
-            <Stack.Screen
-                name="contact"
-                component={ContactScreen}
-                options={{
-                    title: '',
-                    headerLeft: ()=> buttonLeft()
-                }}
-            />
-            <Stack.Screen
-                name="comments"
-                component={SugerenceScreen}
-                options={{
-                    title: '',
-                    headerLeft: ()=> buttonLeft()
-                }}
-            />
-            <Stack.Screen
-                name="donations"
-                component={DonationScreen}
-                options={{
-                    title: '',
-                    headerLeft: ()=> buttonLeft()
-                }}
-            />
-        </Stack.Navigator>
-    )
+      <IconButton
+        icon="menu"
+        onPress={() => navigation.openDrawer()}
+        color="#4E73DF"
+      />
+    );
+  };
+
+  return (
+    <Stack.Navigator initialRouteName="donations">
+      <Stack.Screen
+        name="home"
+        component={HomeScreen}
+        options={{
+          title: '',
+          headerLeft: () => buttonLeft(),
+        }}
+      />
+      <Stack.Screen
+        name="exhibits"
+        component={ExhibitsScrn}
+        options={{
+          title: '',
+          headerLeft: () => buttonLeft(),
+        }}
+      />
+      <Stack.Screen
+        name="events"
+        component={EventsScreen}
+        options={{
+          title: '',
+          headerLeft: () => buttonLeft(),
+        }}
+      />
+      <Stack.Screen
+        name="covid"
+        component={CovidScreen}
+        options={{
+          title: '',
+          headerLeft: () => buttonLeft(),
+        }}
+      />
+      <Stack.Screen
+        name="contact"
+        component={ContactScreen}
+        options={{
+          title: '',
+          headerLeft: () => buttonLeft(),
+        }}
+      />
+      <Stack.Screen
+        name="comments"
+        component={SugerenceScreen}
+        options={{
+          title: '',
+          headerLeft: () => buttonLeft(),
+        }}
+      />
+      <Stack.Screen
+        name="donations"
+        component={DonationScreen}
+        options={{
+          title: '',
+          headerLeft: () => buttonLeft(),
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
 
-const styles = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({});
--- src/screens/DonationScreen.js
+++ src/screens/DonationScreen.js
@@ -1,14 +1,172 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, View, ScrollView} from 'react-native';
+import {
+  Text,
+  Divider,
+  Image,
+  ButtonGroup,
+  Input,
+  CheckBox,
+  Button,
+} from 'react-native-elements';
 
 const DonationScreen = () => {
-    return (
-        <View>
-            <Text>Donation Screen</Text>
+  const [indexQuantity, setIndexQuantity] = React.useState(0);
+  const [indexPeriod, setIndexPeriod] = React.useState(0);
+  const [checked, setChecked] = React.useState(false);
+
+  //Event Handlers
+  const updateIndexQuantity = (selectedIndex) => {
+    setIndexQuantity(selectedIndex);
+  };
+
+  const updateIndexPeriod = (selectedIndex) => {
+    setIndexPeriod(selectedIndex);
+  };
+
+  const updateChecked = (isChecked) => {
+    setChecked(isChecked);
+  };
+
+  const quantityButtons = ['$1', '$2', '$5', '$10', '$20'];
+  const periodButtons = ['Unica', 'Mensual'];
+
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Text h3 h3Style={styles.h3Style}>
+          Haz tu Donación
+        </Text>
+      </View>
+      <Divider style={{backgroundColor: 'blue'}} />
+      <View style={styles.rawContainer}>
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={require('../assets/logo.gif')} />
         </View>
-    )
-}
+      </View>
+      <View style={styles.rawContainer}>
+        <ButtonGroup
+          onPress={updateIndexPeriod}
+          selectedIndex={indexPeriod}
+          buttons={periodButtons}
+          containerStyle={{height: 50, borderRadius: 10}}
+        />
+      </View>
+      <View style={styles.rawContainer}>
+        <ButtonGroup
+          onPress={updateIndexQuantity}
+          selectedIndex={indexQuantity}
+          buttons={quantityButtons}
+          containerStyle={{height: 50, borderRadius: 10}}
+        />
+      </View>
+      <View style={styles.rawContainer}>
+        <Input
+          placeholder="Otra Cantidad"
+          leftIcon={{type: 'font-awesome', name: 'usd'}}
+          inputContainerStyle={styles.inputContainerStyle}
+          leftIconContainerStyle={styles.leftIconContainerStyle}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.rawContainer}>
+        <Input
+          label="Nombre Completo"
+          labelStyle={styles.labelStyle}
+          inputContainerStyle={styles.inputContainerStyle}
+        />
+      </View>
+      <View style={styles.rawContainer}>
+        <Input
+          label="Correo Electrónico"
+          labelStyle={styles.labelStyle}
+          inputContainerStyle={styles.inputContainerStyle}
+        />
+      </View>
+      <View style={styles.rawContainer}>
+        <Input
+          label="Documento de Identidad"
+          labelStyle={styles.labelStyle}
+          inputContainerStyle={styles.inputContainerStyle}
+        />
+      </View>
+      <View style={styles.rawContainer}>
+        <Input
+          label="NIT"
+          labelStyle={styles.labelStyle}
+          inputContainerStyle={styles.inputContainerStyle}
+        />
+      </View>
+      <View style={styles.rawContainer}>
+        <CheckBox
+          title="Deseo recibo de donación"
+          checked={checked}
+          onPress={() => updateChecked(!checked)}
+        />
+      </View>
+      <View style={styles.rawContainer}>
+        <Button
+          title="Donar!"
+          titleStyle={styles.buttonTitleStyle}
+          buttonStyle={styles.buttonStyle}
+          containerStyle={styles.buttonContainerStyle}
+        />
+      </View>
+    </ScrollView>
+  );
+};
 
-export default DonationScreen
+export default DonationScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    padding: 10,
+  },
+  rawContainer: {
+    marginVertical: 5,
+  },
+  h3Style: {
+    fontWeight: 'normal',
+    //fontFamily: 'nunito',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  logoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    height: 150,
+  },
+  logo: {width: 150, height: 150},
+  labelStyle: {
+    marginBottom: 15,
+  },
+  inputContainerStyle: {
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderRadius: 10,
+    marginBottom: -15,
+  },
+  leftIconContainerStyle: {
+    backgroundColor: 'silver',
+    width: '10%',
+    borderRadius: 10,
+  },
+  buttonStyle: {
+    backgroundColor: 'crimson',
+    borderRadius: 7,
+    width: 200,
+  },
+  buttonContainerStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  buttonTitleStyle: {
+    fontWeight: 'normal',
+  },
+});
