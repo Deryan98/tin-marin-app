eed56f94a54dcc6df78ba12c02ef1475b5d1b7cf
cjburgos21 <37281082+cjburgos21@users.noreply.github.com>
2021-01-19 18:30:51 -0600

    Implementando Picker a pantalla Contactos

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, Image} from 'react-native';
 import ContactCard from '../components/ContactCard';
 import Colors from '../constants/Colors';
+import { Picker } from '@react-native-picker/picker';
+import {CONTACTS} from '../dummy-data/contacts'
 
 
 const ContactScreen = ({ navigation }) => {
+  const [contactEmail, setContactEmail] = useState('cambialo');
   return (
     <ScrollView showsVerticalScrollIndicator={true}>
       <View style={styles.view}>
           <Text style={styles.title}>Información de Contactos</Text>
           <View style={styles.line}></View>
+          <Picker
+                style={{ height: 50, width: 250, textAlign: 'center'}}
+                mode = "dropdown"
+                selectedValue={contactEmail}  
+                onValueChange={(itemValue, itemPosition) =>  {
+                  setContactEmail(itemValue)
+                }}  
+            >
+              {
+                CONTACTS.map((contact) => (
+                  <Picker.Item label={contact.charge} value={contact.email} /> 
+                ))
+              }
+            </Picker>
+            {CONTACTS.map((contact) => (contact.email === contactEmail && 
             <ContactCard
-            charge="Visita Famiiar"
+            charge={contact.charge}
             color={Colors.blueColor}
-            name="Karla Lemus"
-            email="correodeprueba@correo.com"
+            name={contact.name}
+            email= {contact.email}
           />
-
+            ))}
       </View>
     </ScrollView>
   );
