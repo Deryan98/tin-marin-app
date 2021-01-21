d5567e0a107eeefd9ded1fa441ccbec62ad60908
cjburgos21 <37281082+cjburgos21@users.noreply.github.com>
2021-01-21 14:16:51 -0600

    agregando pantalla mision y vision y actualizacion de contactos

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -9,10 +9,11 @@ import {CONTACTS} from '../dummy-data/contacts'
 const ContactScreen = ({ navigation }) => {
   const [contactEmail, setContactEmail] = useState('cambialo');
   return (
-    <ScrollView showsVerticalScrollIndicator={true}>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={true}>
       <View style={styles.view}>
           <Text style={styles.title}>Información de Contactos</Text>
           <View style={styles.line}></View>
+          <View style={styles.containerlogo}><Image style={styles.Logo} source={{ uri: 'https://tinmarin.org/wp-content/uploads/2020/04/logo-tm-con-marco-blanco.png', }} /></View>
           <Picker
                 style={{ height: 50, width: 250, textAlign: 'center'}}
                 mode = "dropdown"
@@ -28,7 +29,7 @@ const ContactScreen = ({ navigation }) => {
               }
             </Picker>
             {CONTACTS.map((contact) => (contact.email === contactEmail && 
-            <ContactCard
+            <ContactCard 
             charge={contact.charge}
             color={Colors.blueColor}
             name={contact.name}
@@ -56,7 +57,22 @@ const styles = StyleSheet.create({
         height: 1,
     width: '90%',
     backgroundColor: '#D5D8DC'
-  }
+  },
+  Logo: {
+    width: 200,
+    height: 200
+  },
+  containerlogo: {
+    alignItems: 'center',
+    //backgroundColor: "#ffffff",
+    padding: 15,
+    fontFamily: 'NunitoSans-Bold'
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#ffffff"
+  },
 });
 
 export default ContactScreen;
--- src/screens/MissionAndVisionScreen.js
+++ src/screens/MissionAndVisionScreen.js
@@ -1,14 +1,81 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image, ImageBackground } from 'react-native'
 
 const ContactScreen = () => {
     return (
-        <View>
-            <Text>Pantalla Vision Mision</Text>
-        </View>
-    )
-}
-
-export default ContactScreen
-
-const styles = StyleSheet.create({})
+        <ScrollView style={styles.container}>
+            <Text style={styles.titular}>Conócenos</Text>
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+              }} />
+            <View style={styles.containerlogo}><Image style={styles.Logo} source={{ uri: 'https://tinmarin.org/wp-content/uploads/2020/04/logo-tm-con-marco-blanco.png', }} /></View>
+            <Text style={styles.tema}>Misión:</Text>
+            <Text style={styles.title2}>Contribuir a la formación de los niños y sus acompañantes en ciudadanos integrales y creativos, mediante el aprendizaje significativo, la orientación cultural y la experimentración divertida con integridad, innovación y trabajo en equipo.</Text>
+            <Text style={styles.tema}>Visión:</Text>
+            <Text style={styles.title2}>Ser el espacio favorito, cultural y divertido de los niños para aprender y disfrutar de experiencias únicas, emocionantes e inolvidables; y ser mejores protagonistas de su desarrollo a nivel personal, escolar y social.</Text>
+            <Text style={styles.tema}>Valores:</Text>
+            <Text style={styles.title}>
+              Innovación{"\n"}
+          Integridad{"\n"}
+          Lealtad{"\n"}
+          Respeto{"\n"}
+          Trabajo en equipo{"\n"}
+          </Text>
+            
+          </ScrollView>
+      );
+    }
+    export default ContactScreen
+  
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: "#ffffff"
+      },
+      title2: {
+        marginTop: 16,
+        paddingVertical: 8,
+        borderRadius: 6,
+        backgroundColor: "#ffffff",
+        justifyContent: "center",
+        color: "#20232a",
+        textAlign: "justify",
+        fontFamily: 'NunitoSans-Bold',
+        fontSize: 18,
+      },
+      title: {
+        marginTop: 16,
+        paddingVertical: 8,
+        borderRadius: 6,
+        backgroundColor: "#ffffff",
+        justifyContent: "center",
+        color: "#20232a",
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: "bold"
+      },
+      tema: {
+        marginTop: 13,
+        textAlign: "center",
+        fontFamily: 'NunitoSans-Bold',
+        fontSize: 24,
+        color: "#B22222"
+      },
+      Logo: {
+        width: 200,
+        height: 200
+      },
+      containerlogo: {
+        alignItems: 'center',
+        backgroundColor: "#ffffff",
+        padding: 15
+      },
+      titular: {
+        textAlign: "center",
+        fontFamily: 'NunitoSans-Bold',
+        fontSize: 28
+      },
+    });
