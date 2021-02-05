aee660e42ff39b0b6825b73671f053cbf339e856
DavidVallecios <00078217@uca.edu.sv>
2021-02-04 22:14:57 -0600

    agregando documentacion

--- src/components/ModalBody.js
+++ src/components/ModalBody.js
@@ -12,6 +12,15 @@ import {
 
 import MContent from './Modal';
 
+/**
+ * Componente que muestra el dato curioso de la exhibición.
+ * @param {function} showModal - Método de acceso indirecto para modificar la propiedad modal
+ * @param {boolean} visible - Parametro que indica la visibilidad del componente <Modal />
+ * @param {string} curiousInfo - Dato curioso de la exhibición.
+ * @listens {onPress} | El método showModal se dispara cuando ocurre este evento en un componente <ModalButton>.
+ * @see https://github.com/jacklam718/react-native-modals
+ * @return {Modal} Retorna un componente que contiene maquetada la vista
+ */
 const ModalBody = ({ showModal, visible, curiousInfo }) => {
   return (
     <Modal
@@ -37,4 +46,7 @@ const ModalBody = ({ showModal, visible, curiousInfo }) => {
 
 export default ModalBody;
 
+/**
+ * @ignore
+ */
 const styles = StyleSheet.create({});
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -14,6 +14,27 @@ import { getExhibitionByName } from '../api/exhibitions';
 import Card from '../components/Card';
 import Colors from '../constants/Colors';
 
+/**
+ * Pantalla que muestra una lista de tarjetas con enlaces a pantallas para conocer más sobre Tin Marín.
+ * @param {prop} navigation - Contiene información básica de navegación
+ * @property {function} useEffect - Hook de React que permite realizar tareas asíncronas a la vista.
+ * @property {Promise} getExhibitionByName - {@link getExhibitionByName} | Promesa que devuelve la información dependiendo la respuesta del servidor.
+ * @property {boolean} search - Variable auxiliar que almacena el texto escribo en la barra de busqueda.
+ * @property {function} setSearch - Método de acceso indirecto para modificar la propieadad search.
+ * @property {Object} exhibition - Objeto que contiene la estructura de una Exhibición.
+ * @property {function} setExhibition - Método de acceso indirecto para modificar la propieadad exhibition.
+ * @property {boolean} loading - Variable auxiliar que indica si la busqueda se ha completado o no.
+ * @property {function} setLoading - Método de acceso indirecto para modificar la propieadad loading.
+ * @property {boolean} swapColor - Variable auxiliar para cambiar color de tarjetas cada posición: (índice mod 2).
+ * @listens {onChange} | El método onChangeSearch se dispara cuando ocurre este evento en un componente <SearchBar>.
+ * @property {function} size - Función de la librería lodash | Devuelve el tamaño de una colección.
+ * @property {function} map - Función de la librería lodash | Crea un arreglo de valores a partir de cada elemento de una colección.
+ * @see https://reactnavigation.org/docs/navigation-prop/
+ * @see https://lodash.com/docs/4.17.15#size
+ * @see https://lodash.com/docs/4.17.15#map
+ * @see https://callstack.github.io/react-native-paper/searchbar.html
+ * @return {SafeAreaView} Retorna un componente que contiene maquetada la vista
+ */
 const Search = ({ navigation }) => {
   const [search, setSearch] = useState('');
   const [exhibitions, setExhibitions] = useState(null);
